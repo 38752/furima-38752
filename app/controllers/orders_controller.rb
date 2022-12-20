abd29904ad5e0910db458f0b564525c010e07bce
38752 <118719220+38752@users.noreply.github.com>
2022-12-20 12:24:38 +0900

    Formオブジェクトを用いた購入情報の登録機能

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,25 @@
 class OrdersController < ApplicationController
   def new
+    @item = Item.find(params[:id])
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      order_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:id])
+      render :new
+    end
+  end
+
+  private
+
+  def order_params
+    params.permit(
+      :post_code, :prefecture_id, :city, :address, :apartment, :phone_number
+    ).merge(user_id: current_user.id, item_id: params[:id])
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -20,5 +20,5 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
-  # has_one :purchase
+  has_one :order
 end
