6d404e1ebeec9fe58f586caf07438f347d656719
38752 <118719220+38752@users.noreply.github.com>
2022-12-20 17:29:14 +0900

    Rubocopをもとにした細かな修正

--- spec/factories/order_addresses.rb
+++ spec/factories/order_addresses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :order_address do
-    token         {"tok_abcdefghijk00000000000000000"}
-    post_code     { Faker::Number.number(digits: 3).to_s + '-' + Faker::Number.number(digits: 4).to_s }
+    token         { "tok_abcdefghijk00000000000000000" }
+    post_code     { "#{Faker::Number.number(digits: 3)}-#{Faker::Number.number(digits: 4)}" }
     prefecture_id { Faker::Number.within(range: 2..48) }
     city          { Faker::Address.city }
     address       { Faker::Address.secondary_address }
